99a2797594fdad06bcb5cf4cda2e80d8c1fcfdd6
Zaeem4545 <156640302+Zaeem4545@users.noreply.github.com>
2026-01-06 18:31:50 +0500

    change ip to fqdn revert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // Production FQDN - API calls will be proxied through nginx
-  // Using relative path '/api' so it works with https://projects.expertflow.com/
-  apiBase: '/api'
+  // This will be injected by Docker build arg
+  apiBase: 'http://host.docker.internal:3000'
 };
